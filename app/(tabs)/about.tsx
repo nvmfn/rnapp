48a4b4645548f2f04acc076c0adfee07e85094db
TrapenokVictor <Levhav@yandex.com>
2024-11-05 09:14:05 +0500

    VideoCall code for monday

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,11 @@
-import { Text, View, StyleSheet } from 'react-native';
+import VideoCall from "@/components/VideoCalls/VideoCall";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About screen</Text>
+      <VideoCall />
     </View>
   );
 }
@@ -11,11 +13,11 @@ export default function AboutScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
-    color: '#fff',
+    color: "#fff",
   },
 });
